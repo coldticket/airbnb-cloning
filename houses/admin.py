27a0dd63a848353e49050e2f0e38b058e47ee4fc
coldticket <121993129+coldticket@users.noreply.github.com>
2023-02-22 18:58:06 +0900

    delete house

--- houses/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import House
-@admin.register(House)
-class HouseAdmin(admin.ModelAdmin):
-    list_display = ["name", "price", "smoking_allowed","address"]
-    list_filter = ["price","smoking_allowed"]
-    search_fields = ["address"]
